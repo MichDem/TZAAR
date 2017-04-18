62f86b35238a22e72572fe6deab57196dcc52dca
MiKu <10mkudela@gmail.com>
2017-04-18 22:31:31 +0200

    Main class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import gui.MainWindow;
+
+import java.awt.*;
+
 /**
+ * Starts up a progam
+ * And... that's it
  * Created by miku on 11.04.17.
  */
 public class Main {
+
+    public static void main(String[] args){
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MainWindow.getInstance().setVisible(true);
+            }
+        });
+    }
 
 }
