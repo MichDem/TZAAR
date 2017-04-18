614f594abcccc90f33366ddb05a836339f8c8906
MiKu <10mkudela@gmail.com>
2017-04-18 22:24:07 +0200

    Extended Existing classes Commented out unused code

--- src/game/Piece.java
+++ src/game/Piece.java
@@ -1,21 +1,23 @@
 package game;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
+ * Class used to represent Piece
  * Created by miku on 11.04.17.
  */
 public class Piece {
 
-    private Board.Field position;   //FIXME to remove
+    //private Board.Field position;
 
-    public boolean isFree() {
+    /*public boolean isFree() {
         return free;
     }
 
     public void setFree(boolean free) {
         this.free = free;
+    }*/
+
+    public Color getColor() {
+        return color;
     }
 
     public enum Type{
@@ -24,36 +26,37 @@ public class Piece {
 
     private Type type;
     private Color color;
-    private boolean free;
-    private List<Piece> addedPieces;
+    //private boolean free;
+    //private List<Piece> addedPieces;
     private int power;
 
-    public Piece(Type type, Color color){
+    Piece(Type type, Color color){
         this.type = type;
         this.color = color;
         power = 1;
-        free = true;
-        addedPieces = new ArrayList<>(0);
+        //free = true;
+        //addedPieces = new ArrayList<>(0);
     }
 
     public void add(Piece another){
         power += another.power;
-        addedPieces.add(another);
+        //addedPieces.add(another);
+        //this.power += another.power;
         another.power = 0;
-        another.free = false;
+        //another.free = false;
     }
 
-    public void hit(Piece another){
-        addedPieces.add(another);
+    void hit(Piece another){
         another.power = 0;
-        another.free = false;
+        //another.free = false;
+    }
+
+    public int getPower(){
+        return power;
     }
 
     public Type getType(){
         return type;
     }
-
-    //TODO metoda generateMove
-    //TODO metoda checkMove
 
 }
